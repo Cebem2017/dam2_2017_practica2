b9d458eec3d1d8438e20984cd695a3b2b025f508
araceli24 <aracelilores@gmail.com>
2017-11-21 10:10:25 +0100

    programa que dibuja un triangulo

---
+++ AraceliLores.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+home()
+shape("turtle") # cambiamos la forma a Tortuga
+pensize(10)
+pencolor("blue")
+left(60) 
+forward(200) 
+right(120) 
+forward(200) 
+right(120)
+forward(200)  
+exitonclick() 
